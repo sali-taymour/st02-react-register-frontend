e7f9e34003f92a5ee09127d88983f5650f887041
sali <sali.taymor@gmail.com>
2022-08-24 00:09:21 +0200

    tow pages are protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { NavLink, Route, Routes, Navigate } from "react-router-dom";
+import { IUser } from "./interfaces";
+import { Page404 } from "./pages/Page404";
 
 import { PageWelcome } from "./pages/PageWelcome";
 import { PageMembers } from "./pages/PageMembers";
@@ -12,24 +16,51 @@ const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 console.log(baseUrl, 'baseUrl');
 function App() {
+
+
+
+    const [currentUser, setCurrentUser] = useState<IUser>({
+        username: "",
+        firstName: "",
+        lastName: "",
+        accessGroups: [],
+    });
+
+    useEffect(() => {
+        (async () => {
+            const data = (await axios.get(`${baseUrl}/current-user`)).data;
+            const _currentUser = data.currentUser;
+            setCurrentUser(_currentUser);
+        })();
+    }, []);
     return (
         <div className="App">
             <h1>Language Tandem Group</h1>
+            <h1>{currentUser.username}</h1>
             <p>testing</p>
             <nav>
                 <NavLink to="/welcome">Welcome</NavLink>
-                <NavLink to="/members">Members</NavLink>
+                {currentUser.accessGroups.includes("members") && (
+                    <NavLink to="/members">Members</NavLink>
+                )}
                 <NavLink to="/register">Register</NavLink>
                 <NavLink to="/login">Login</NavLink>
-                <NavLink to="/logout">Logout</NavLink>
+                {currentUser.accessGroups.includes("loggedInUsers") && (
+                    <NavLink to="/logout">Logout</NavLink>
+                )}
             </nav>
 
             <Routes>
+                <Route path="*" element={<Page404 />} />
                 <Route path="/welcome" element={<PageWelcome />} />
-                <Route path="/members" element={<PageMembers />} />
+                {currentUser.accessGroups.includes("members") && (
+                    <Route path="/members" element={<PageMembers />} />
+                )}
                 <Route path="/register" element={<PageRegister />} />
                 <Route path="/login" element={<PageLogin />} />
-                <Route path="/logout" element={<PageLogout />} />
+                {currentUser.accessGroups.includes("loggedInUsers") && (
+                    <Route path="/logout" element={<PageLogout />} />
+                )}
                 <Route path="/confirm-link" element={<PageConfirmLink />} />
                 <Route path="/" element={<Navigate to="/welcome" replace />} />
             </Routes>
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,7 @@
+export const Page404 = () => {
+    return (
+        <div className="page404">
+            <p>404 Page not found</p>
+        </div>
+    );
+};
